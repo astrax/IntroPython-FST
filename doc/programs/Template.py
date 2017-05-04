94d40c3be9d114ae695299ba090ef4663bd76f99
astrax <ammarahmed.ph@gmail.com>
2017-05-04 22:43:31 +0100

    First commit

---
+++ doc/programs/Template.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 20:00:55 2017
+
+@author: Ahmed
+"""
+
---
+++ doc/programs/loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+n = 100
+s = 0
+counter = 1
+
+while counter <= n:
+    s = s + counter
+    counter += 1
+print("Sum of 1 until %d: %d" % (n,s))
